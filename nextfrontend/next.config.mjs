af6be20cead181f550457977afd4caa9525a5f17
unknown <ashalhat10000@gmail.com>
2025-04-23 15:01:50 +0530

    next fix

--- nextfrontend/next.config.mjs
+++ nextfrontend/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com','res.cloudinary.com'],
-      },
+  images: {
+    domains: ['images.unsplash.com', 'res.cloudinary.com'],
+  },
+  output: 'export',
 };
 
 export default nextConfig;
